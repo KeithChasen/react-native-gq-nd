95fc86951fe35c8f7608fb2f968a8209f9b071f2
keithchasen <keithchasen89@gmail.com>
2020-09-11 23:47:38 +0300

    text inputs / buttons and state

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const clearHandler = () => {
+    setName('');
+    setAge('');
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Body</Text>
-      </View>
+      <Text>Enter name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Name'
+        onChangeText={(val) => setName(val)}
+        value={name}
+      />
+      <Text>Enter age:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        placeholder='Age'
+        onChangeText={(val) => setAge(val)}
+        value={age}
+      />
+      <Text>My name is {name}</Text>
+      <Text>My age is {age}</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='Clear' onPress={clearHandler} />
+        </View>
     </View>
   );
 }
@@ -22,15 +43,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20
+  buttonContainer: {
+    marginTop: 20
   },
-  boldText: {
-    fontWeight: 'bold'
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200
   }
 });

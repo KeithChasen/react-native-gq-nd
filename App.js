b9a9faf73a9680fd8ef1ef2134e1b88befe33a3b
keithchasen <keithchasen89@gmail.com>
2020-09-13 16:44:05 +0300

    set up for review app: basic screens

--- App.js
+++ App.js
@@ -1,71 +1,9 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import Header from "./components/header";
-import TodoItem from "./components/todoItem";
-import AddTodo from "./components/addTodo";
+import React from 'react';
+import Home from "./screens/home";
 
 export default function App() {
-  const [list, setList] = useState([
-    { text: 'Learn React Native', id: 1 },
-    { text: 'Learn GraphQL', id: 2 },
-    { text: 'Make an awesome project', id: 3 },
-  ]);
-
-  const pressHandler = (id) => {
-    setList(prevList => prevList.filter(item => item.id !== id))
-  };
-
-  const submitHandler = text => {
-    if (text.length > 3) {
-      setList(prevList => ([
-        ...prevList,
-        {
-          text: text,
-          id: Math.random().toString()
-        }
-      ]))
-    } else {
-      Alert.alert('Whoops!', 'Text should be over 3 characters long', [
-        { text: 'Ok', onPress: () => console.log('button pressed')}
-      ])
-    }
-
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-    }}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              keyExtractor = { (item) => item.id }
-              data={list}
-              renderItem={({ item }) => (
-                <TodoItem item={item} pressHandler={pressHandler}/>
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+    <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  content: {
-    padding: 40,
-    flex: 1
-  },
-  list: {
-    marginTop: 20,
-    flex: 1
-  }
-});
---
+++ screens/home.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24
+  }
+});
